c01ca40fd28c0d3fa1982f7673838dfe7e05f5ce
dddddddaaa87 <dddddddaaa87@mail.ru>
2023-05-25 16:30:19 +0300

    Починил свагер

--- CarPool/Startup.cs
+++ CarPool/Startup.cs
@@ -67,14 +67,15 @@ namespace CarPool
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
+            //
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarPool v1"));
-            }
+            //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
